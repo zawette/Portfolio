ca543b57bf872cfe6a9b90562a47f542f885bbac
zawette <zawette@gmail.com>
2021-10-03 23:52:32 +0100

    truncate text

--- src/components/Blog/Post/PostStyles.js
+++ src/components/Blog/Post/PostStyles.js
@@ -1,37 +1,49 @@
 import styled from "styled-components";
 
 export const PostContainer = styled.div`
-background-color: ${(props)=>props.theme.colors.background2};
-padding : 2rem;
-margin-top:2rem;
+  background-color: ${(props) => props.theme.colors.background2};
+  padding: 2rem;
+  margin-top: 2rem;
 `;
 
 export const PostTitle = styled.div`
-color:${(props)=>props.theme.colors.primary1};
-font-size: 2em;
-`
+  font-weight: 500;
+  background: linear-gradient(121.57deg, #FFFFFF 18.77%, rgba(255, 255, 255, 0.66) 60.15%);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  color: ${(props) => props.theme.colors.primary1};
+  font-size: 2em;
+  @media ${props => props.theme.breakpoints.md}{
+  }
 
-export const PostHeader = styled.div`
+  @media ${props => props.theme.breakpoints.sm}{
+  }
+`;
 
-`
+export const PostHeader = styled.div``;
 
 export const PostTags = styled.a`
-text-decoration: underline;
-margin-right: 1rem;
-font-size: 0.7em;
-&:hover{
-    cursor:pointer;
-}
-`
+  text-decoration: underline;
+  margin-right: 1rem;
+  font-size: 0.7em;
+  &:hover {
+    cursor: pointer;
+  }
+`;
 export const PostDate = styled.span`
-margin-right: 1rem;
-font-size: 0.7em;
-`
+  margin-right: 1rem;
+  font-size: 0.7em;
+`;
 
 export const PostExcerpt = styled.div`
-font-size: 1em;
-width: 75rem;
-white-space: nowrap;
-overflow: hidden;
-text-overflow: ellipsis;
-`
+  font-size: 1em;
+  overflow:hidden;
+  line-height: 2rem;
+  max-height: 8rem;
+  -webkit-box-orient: vertical;
+  display: block;
+  display: -webkit-box;
+  overflow: hidden !important;
+  text-overflow: ellipsis;
+  -webkit-line-clamp: 2;
+`;
